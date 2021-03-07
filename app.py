826fb10ae3683f562f619f073639482accddaff4
mbscale <68180018+mbscale@users.noreply.github.com>
2021-03-07 16:14:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask!'
+    return 'Web App with Python Flask!!'
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+app.run(host='0.0.0.0', port=80)
